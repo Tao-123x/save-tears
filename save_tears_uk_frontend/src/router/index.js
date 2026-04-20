f76702954557e36eac8bbeeb09d19bce64f0bb54
🅰️manda <amandakoka@icloud.com>
2026-04-20 20:04:41 +0100

    Update routing and views when logged in.

--- save_tears_uk_frontend/src/router/index.js
+++ save_tears_uk_frontend/src/router/index.js
@@ -4,11 +4,31 @@ import Login from '../views/pages/Login.vue'
 import Register from '../views/pages/Register.vue'
 import Dashboard from '../views/pages/Dashboard.vue'
 
+// checks if user is authenticated and logged in, if not redirects to login page
+const ifAuthenticated = (to, from, next) => {
+    const loggedIn = localStorage.getItem("token");
+    if (loggedIn) {
+        next();
+        return;
+    }
+    next("/login");
+}
+
+const ifNotAuthenticated = (to, from, next) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    next("/dashboard");
+  } else {
+    next();
+  }
+};
+
 const routes = [
   { path: '/', component: Home},
-  { path: '/login', component: Login},
-  { path: '/register', component: Register },
-  { path: '/dashboard', component: Dashboard }
+  { path: '/login', component: Login, beforeEnter: ifNotAuthenticated },
+  { path: '/register', component: Register, beforeEnter: ifNotAuthenticated },
+  { path: '/dashboard', component: Dashboard, beforeEnter: ifAuthenticated }
 ]
 
 const router = createRouter({
--- save_tears_uk_frontend/src/views/App.vue
+++ save_tears_uk_frontend/src/views/App.vue
@@ -4,9 +4,12 @@
       <img alt="Save Tears logo" class="logo" src="../static/images/logo.png"/>
       <ul>
         <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/login">Login</router-link></li>
-        <li><router-link to="/register">Register</router-link></li>
-        <li><router-link to="/dashboard">Dashboard</router-link></li>
+        <li><router-link v-if="!isLoggedIn" to="/login">Login</router-link></li>
+        <li><router-link v-if="!isLoggedIn" to="/register">Register</router-link></li>
+        <li><router-link v-if="isLoggedIn"to="/dashboard">Dashboard</router-link></li>
+        <li v-if="isLoggedIn">
+            <button @click="logout">Logout</button>
+        </li>
       </ul>
     </nav>
   </header>
@@ -18,7 +21,19 @@
 
 <script>
 export default {
-  name: 'App'
-}
+  name: 'App',
 
+  data() {
+    return {
+      isLoggedIn: !!localStorage.getItem('token')
+    }
+  },
+  watch: {
+    // Watch for changes in localStorage to update isLoggedIn status
+    '$route'(to, from) {
+      this.isLoggedIn = !!localStorage.getItem('token');
+    }
+  }
+
+}
 </script>
